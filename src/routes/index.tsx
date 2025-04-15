a1978f39d7f5c8c3fbb4ccad0634ca1b1d43b812
prajun <prajunbudathoki1@gmail.com>
2025-04-15 06:15:57 +0000

    initializing and linking up the route using tanstack router

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import { createFileRoute, Link } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: Home,
+})
+
+function Home() {
+  return (
+    <div className="p-2">
+      <h3>Welcome Home!!!</h3>
+      <Link to='/testroute'>
+          Naviagte to testroute
+      </Link>
+    </div>
+  )
+}
